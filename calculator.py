4ffd7940f997fc0e4052c1a01a33a753ca502f3f
edlance1 <edlance@ntiertraining.com>
2026-07-12 12:12:35 -0400

    modified add function to fail test

--- calculator.py
+++ calculator.py
@@ -2,9 +2,8 @@
 
 def add (a, b):
     # A simple function that adds two numbers together
-    return a + b
+    return a + b + 99
 
 if __name__ == "__main__":
     print(f"2 + 3 is equal to {add(2,3)}")
 
-    
